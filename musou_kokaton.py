3146984f7b253ee1171048820e925e36a2803d36
shunki <c0a240267d@edu.teu.ac.jp>
2025-05-13 16:41:44 +0900

    2

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -94,6 +94,7 @@ class Bird(pg.sprite.Sprite):
             if key_lst[k]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+                
             if key_lst[pg.K_LSHIFT]:  # 左Shiftを押しているとき高速化
                 self.speed = 20
             else:
